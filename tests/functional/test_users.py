b84152e27b1e58289fe0d17d3f9b4673d7db0618
Yaroslav Khoruzhenko <yakhoruzhenko@gmail.com>
2025-02-19 10:41:19 +0200

    Adding auth tests, small API adjustments

--- tests/functional/test_users.py
+++ tests/functional/test_users.py
@@ -4,9 +4,6 @@ from tests import datetime_to_str, random_string
 
 from app.schemas.user import UserResponseWithChecks
 
-# Test cases for invalid user input are not included, as such cases are already covered by FastAPI and Pydantic
-# validation!
-
 
 def test_create_user_success(test_client: TestClient) -> None:
     name = random_string()
@@ -25,24 +22,22 @@ def test_create_user_success(test_client: TestClient) -> None:
         assert response_json['email'] == email
 
 
-def test_create_user_already_exists(test_client: TestClient, get_seeded_user: UserResponseWithChecks) -> None:
+def test_create_user_already_exists(test_client: TestClient, seeded_user: UserResponseWithChecks) -> None:
     with test_client:
         response = test_client.post(url='/users', json=dict(name=random_string(),
-                                                            login=get_seeded_user.login,
+                                                            login=seeded_user.login,
                                                             password=random_string(),
                                                             email=f'{random_string()}@gmail.com'))
 
         assert response.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY
         assert response.json() == \
-            {'detail': f'User with login: {get_seeded_user.login} already exists'}
+            {'detail': f'User with login: {seeded_user.login} already exists'}
 
 
-def test_get_current_user_profile_success(test_client: TestClient, get_seeded_user: UserResponseWithChecks) -> None:
+def test_get_current_user_profile_success(test_client: TestClient, seeded_user: UserResponseWithChecks) -> None:
     with test_client:
 
-        auth_response = test_client.post(url='/login',
-                                         data=dict(username=get_seeded_user.login,
-                                                   password='password123'))
+        auth_response = test_client.post(url='/login', data=dict(username=seeded_user.login, password='password123'))
 
         assert auth_response.status_code == status.HTTP_200_OK
 
@@ -52,10 +47,10 @@ def test_get_current_user_profile_success(test_client: TestClient, get_seeded_us
 
         assert get_user_profile_response.status_code == 200
         get_user_profile_response_json = get_user_profile_response.json()
-        assert get_user_profile_response_json['id'] == str(get_seeded_user.id)
-        assert get_user_profile_response_json['name'] == get_seeded_user.name
-        assert get_user_profile_response_json['login'] == get_seeded_user.login
-        assert get_user_profile_response_json['email'] == get_seeded_user.email
-        assert get_user_profile_response_json['created_at'] == datetime_to_str(get_seeded_user.created_at)
+        assert get_user_profile_response_json['id'] == str(seeded_user.id)
+        assert get_user_profile_response_json['name'] == seeded_user.name
+        assert get_user_profile_response_json['login'] == seeded_user.login
+        assert get_user_profile_response_json['email'] == seeded_user.email
+        assert get_user_profile_response_json['created_at'] == datetime_to_str(seeded_user.created_at)
         assert len(get_user_profile_response_json['checks']) == 1
         assert len(get_user_profile_response_json['checks'][0]['items']) == 2
