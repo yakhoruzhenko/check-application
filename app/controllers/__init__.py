1a84d1ff50e072131c5061dc42006273aa0c77fb
Yaroslav Khoruzhenko <yakhoruzhenko@gmail.com>
2025-02-24 18:10:16 +0200

    Adjusting Field examples, replacing Query with Select

--- app/controllers/__init__.py
+++ app/controllers/__init__.py
@@ -6,6 +6,6 @@ from pydantic import AnyHttpUrl
 from pydantic_core import Url
 
 
-def get_response_url(request: Request, id: UUID) -> Url:
+def get_response_url(request: Request, id: UUID) -> AnyHttpUrl:
     path = request.app.url_path_for('get_check_text_repr_by_id', id=id)
     return AnyHttpUrl(str(request.base_url.replace(path=str(path))))
--- app/repositories/check.py
+++ app/repositories/check.py
@@ -4,6 +4,7 @@ from uuid import UUID
 from fastapi import HTTPException
 from fastapi_pagination import Page, Params
 from fastapi_pagination.ext.sqlalchemy import paginate
+from sqlalchemy import select
 from sqlalchemy.orm import Session
 from starlette import status
 
@@ -26,21 +27,21 @@ def create(db: Session, schema: check.CreateCheck) -> checks.Check:
 def get_all_by_user(db: Session, creator_id: UUID, pagination_params: Params,
                     filters: check.CheckFilters) -> Page[checks.Check]:
     # Don't call 'all' method to avoid loading all query results into the memory
-    query = db.query(checks.Check).filter(checks.Check.creator_id == creator_id)
+    stmt = select(checks.Check).filter(checks.Check.creator_id == creator_id)
     if filters.period_start:
-        query = query.filter(checks.Check.created_at >= filters.period_start)
+        stmt = stmt.filter(checks.Check.created_at >= filters.period_start)
     if filters.period_end:
         # adding timdelta is needed because at the comparing time date is converted to timestamp of such format:
         # 2025-02-14 00:00:00, so when both dates are i.e. 2025-02-14, Postgres will look up for all checks
         # that were create between 2025-02-14 00:00:00 and 2025-02-14 00:00:00
-        query = query.filter(checks.Check.created_at < filters.period_end + timedelta(days=1))
+        stmt = stmt.filter(checks.Check.created_at < filters.period_end + timedelta(days=1))
     if filters.total_amount_ge:
-        query = query.filter(checks.Check.total_amount >= filters.total_amount_ge)
+        stmt = stmt.filter(checks.Check.total_amount >= filters.total_amount_ge)
     if filters.total_amount_le:
-        query = query.filter(checks.Check.total_amount <= filters.total_amount_le)
+        stmt = stmt.filter(checks.Check.total_amount <= filters.total_amount_le)
     if filters.payment_method:
-        query = query.filter(checks.Check.payment_method == filters.payment_method)
-    return paginate(query, params=pagination_params)  # type: ignore
+        stmt = stmt.filter(checks.Check.payment_method == filters.payment_method)
+    return paginate(db, stmt, params=pagination_params)  # type: ignore
 
 
 def get_by_id(db: Session, id: UUID) -> checks.Check:
